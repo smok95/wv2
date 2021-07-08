1a0243c2c411485f5c7222b45333f066e7c494e7
Kim, Jong-kook <kjk15881588@gmail.com>
2021-07-08 13:53:18 +0900

    add example

---
+++ example/example_win32/framework.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
